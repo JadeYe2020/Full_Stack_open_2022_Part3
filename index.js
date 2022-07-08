9ae1328b99d245ac49045f46e29341b76c2291f3
Jade Ye <w0430394@nscc.ca>
2022-07-08 10:02:08 -0300

    Make ready to deploy

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { response } = require('express')
 const express = require('express')
 const { restart } = require('nodemon')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 let persons = [
   { 
@@ -95,6 +97,6 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`);
